9e267b3f0481fc6ffc64f078170f09b181b89c49
MartinStamenkovski <martin@pop-os.localdomain>
2018-12-02 15:57:20 +0100

    code refactor

--- prefsmanager/src/main/java/com/stamenkovski/prefsmanager/PrefsManager.java
+++ prefsmanager/src/main/java/com/stamenkovski/prefsmanager/PrefsManager.java
@@ -2,9 +2,7 @@ package com.stamenkovski.prefsmanager;
 
 import android.content.Context;
 import android.content.SharedPreferences;
-import android.util.Log;
 import com.google.gson.Gson;
-import com.google.gson.reflect.TypeToken;
 
 import java.lang.reflect.Type;
 
